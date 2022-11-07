8fd6ce0168795e18fdd81708287b830749ba3dbe
MCoppinDev <mattcoppinger@protonmail.com>
2022-11-06 21:39:47 -0500

    feature branch added

--- src/main/java/org/launchcode/preplate/PreplateApplication.java
+++ src/main/java/org/launchcode/preplate/PreplateApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PreplateApplication {
+public class PreplateApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PreplateApplication.class, args);
